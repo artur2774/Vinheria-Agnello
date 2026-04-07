e2952a511c1a9c764f674f3cb7f48fa702b96819
Artur Maia <artur.novazzi@outlook.com>
2026-04-06 21:47:02 -0300

    fix: arrumando código JS

--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,15 @@ let cadastroNome = prompt('Insira seu nome');
 alert(`Seja bem vindo ${cadastroNome}`);
 
 let nomeVinho = prompt('Insira o nome do vinho');
-let tipoVinho = prompt('Insria o tipo do vinho \nTinto \nBranco \nRose')
-let safraVinho = prompt('Insira a safra do vinho (ano de produção)')
-let quantidade = prompt('Insira a quantidade em estoque')
+let tipoVinho = prompt('Insria o tipo do vinho \n-Tinto \n-Branco \n-Rose');
+let safraVinho = prompt('Insira a safra do vinho (ano de produção)');
+let quantidade = prompt('Insira a quantidade em estoque');
 
 console.log("╔══════════════════════════════════════╗");
 console.log("║   🍷  VINHARIA AGNELLO – ESTOQUE     ║");
 console.log("╠══════════════════════════════════════╣");
-console.log(║  Nome      : ${nomeVinho});
-console.log(║  Tipo      : ${tipoVinho});
-console.log(║  Safra     : ${safraVinho});
-console.log(║  Estoque   : ${quantidade} unidade(s));
+console.log(`║  Nome      : ${nomeVinho}`);
+console.log(`║  Tipo      : ${tipoVinho}`);
+console.log(`║  Safra     : ${safraVinho}`);
+console.log(`║  Estoque   : ${quantidade} unidade(s)`);
 console.log("╚══════════════════════════════════════╝");
